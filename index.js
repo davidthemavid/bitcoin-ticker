49d2c82892c59d1303db4b20a4bbbd4aa33ae2ff
David <51271545+davidthemavid@users.noreply.github.com>
2020-01-10 16:47:41 -0800

    fixed request to both endpoints; displays crypto conversion & high/low

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// TODO: Figure out how to apply css file to index.html
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -13,12 +15,11 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
   let crypto = req.body.crypto;
   let fiat = req.body.fiat;
-  let URL = "https://apiv2.bitcoinaverage.com/convert/global";
-  //let finalURL = URL + crypto + fiat;
   let amount = req.body.amount;
 
-  let options = {
-    url: URL,
+  //sends crypto value with desired fiat outcome
+  let option1 = {
+    url: "https://apiv2.bitcoinaverage.com/convert/global",
     method: "GET",
     qs: {
       from: crypto,
@@ -27,11 +28,16 @@ app.post("/", function(req, res) {
     }
   };
 
-  request(options, function(error, response, body) {
+  //sends crypto type & fiat to get desired crypto highs and lows
+  let option2 = {
+    url:
+      "https://apiv2.bitcoinaverage.com/indices/global/ticker/" + crypto + fiat,
+    method: "GET"
+  };
+
+  request(option1, function(error, response, body) {
     let data = JSON.parse(body);
     let price = data.price;
-    // let high = data.high;
-    // let low = data.low;
     let todaysDate = data.time;
     res.write("<p>Today's date is: " + todaysDate + "</p>");
     console.log("Status code: " + response.statusCode);
@@ -46,7 +52,14 @@ app.post("/", function(req, res) {
         fiat +
         "</h1>"
     );
-    res.send();
+
+    request(option2, function(error, response, body) {
+      let data2 = JSON.parse(body);
+      let high = data2.high;
+      let low = data2.low;
+      res.write("<h2>High: " + high + " Low: " + low + "</h2>");
+      res.send();
+    });
   });
 });
 
